bbb53fc3dc04a6195901e4e5182ad8f59d27ce74
AamirKT <aamirt@hotmail.co.uk>
2026-06-25 06:04:04 +0100

    add pod-identity.tf

---
+++ terraform/modules/eks/pod-identity.tf
@@ -0,0 +1,107 @@
+data "aws_route53_zone" "main" {
+  zone_id      = var.zone_id
+  private_zone = true
+}
+
+resource "aws_iam_role" "pod_identity_external_dns" {
+  name = "${var.cluster_name}-pod-identity-external-dns"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "pod_identity_external_dns_policy" {
+  name = "${var.cluster_name}-pod-identity-external-dns-policy"
+  role = aws_iam_role.pod_identity_external_dns.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ChangeResourceRecordSets",
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets"
+        ]
+        Resource = "arn:aws:route53:::hostedzone/${data.aws_route53_zone.main.zone_id}"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets",
+          "route53:ListTagsForResource"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_eks_pod_identity_association" "external_dns" {
+  cluster_name    = var.cluster_name
+  namespace       = "external-dns"
+  service_account = "external-dns"
+  role_arn        = aws_iam_role.pod_identity_external_dns.arn
+}
+
+resource "aws_iam_role" "external_secrets_role" {
+  name = "${var.cluster_name}-pod-identity-external-secrets"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "external_secrets_policy" {
+  name = "${var.cluster_name}-pod-identity-external-secrets-policy"
+  role = aws_iam_role.external_secrets_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = var.rds_secret_arn
+      }
+    ]
+  })
+}
+
+resource "aws_eks_pod_identity_association" "external_secrets" {
+  cluster_name    = var.cluster_name
+  namespace       = "external-secrets"
+  service_account = "external-secrets"
+  role_arn        = aws_iam_role.external_secrets_role.arn
+}
