a4b9bde82171d23f53d719f95ec4b2ae499ee82d
AamirKT <aamirt@hotmail.co.uk>
2026-07-02 20:56:59 +0100

    add exec to helm provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -38,6 +38,11 @@ provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks_cluster.token
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_name]
+    }
   }
 }
