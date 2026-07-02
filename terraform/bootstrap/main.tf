4dc3fccd4d3f183f2b299d6f06f02d34b1174294
AamirKT <aamirt@hotmail.co.uk>
2026-07-02 20:11:48 +0100

    add terraform-plan role

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -160,3 +160,38 @@ resource "aws_iam_role_policy" "github_actions_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "terraform_plan" {
+  name = "terraform-plan-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "terraform_plan_readonly" {
+  name       = "terraform-plan-readonly-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  roles      = [aws_iam_role.terraform_plan.name]
+}
+
+  
