f6fade9ba3e100271f6e14f0ba5354bd1311008f
AamirKT <aamirt@hotmail.co.uk>
2026-07-07 02:06:07 +0100

    update traefik version

--- terraform/modules/eks/helm.tf
+++ terraform/modules/eks/helm.tf
@@ -65,7 +65,7 @@ resource "helm_release" "cert_manager" {
 
 resource "helm_release" "traefik" {
   name             = "traefik"
-  version          = "41.0.0"
+  version          = "41.0.2"
   repository       = "https://helm.traefik.io/traefik"
   chart            = "traefik"
   namespace        = "traefik"
